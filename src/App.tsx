c8ea1c2413b8c0f79829f86c419ebb7eb50ee486
jiwonMe <park@jiwon.me>
2024-01-01 20:52:49 +0900

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MobileView from './components/MobileView';
 import MainPage from './pages/MainPage';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
             <Route path="/" element={<MainPage />} />
           </Routes>
         </BrowserRouter>
+        <Analytics />
     </MobileView>
   );
 };
